d16ded710e90ddbcab47ea58f54fa24ca49fa7e9
kanned1079 <kanned1079@gmail.com>
2024-08-12 14:17:13 +0800

    update0812

--- Vue3/nxc-admin-server/router/handles.go
+++ Vue3/nxc-admin-server/router/handles.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"MyFrontEndStudy/Vue3/nxc-admin-server/dao"
+	"MyFrontEndStudy/Vue3/nxc-admin-server/user"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -8,6 +10,15 @@ import (
 
 func handleGetAllUsers(context *gin.Context) {
 	log.Println("GetAllUsers")
+	var nxcUser user.NxcUsers
+	result := dao.Db.Model(&user.NxcUsers{}).Limit(1).Find(&nxcUser)
+	if result.Error != nil {
+		log.Println(result.Error.Error())
+	}
+	var userSlice []user.NxcUsers
+	for _, user := range userSlice {
+		log.Println(user)
+	}
 	context.JSON(http.StatusOK, gin.H{
 		"path": http.StatusOK,
 	})
